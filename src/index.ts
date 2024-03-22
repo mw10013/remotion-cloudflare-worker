c8bcf28e6d1524dc4e413eac726177dc047302a4
Michael Wu <mw10013@gmail.com>
2024-03-22 10:59:52 -0400

    .

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { AwsClient } from 'aws4fetch';
+
 export interface Env {
 	REMOTION_AWS_ACCESS_KEY_ID: string;
 	REMOTION_AWS_SECRET_ACCESS_KEY: string;
@@ -5,11 +7,24 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const result = 'remotion';
-		return new Response(
-			JSON.stringify({
-				result,
-			})
-		);
+		const aws = new AwsClient({
+			accessKeyId: env.REMOTION_AWS_ACCESS_KEY_ID,
+			secretAccessKey: env.REMOTION_AWS_SECRET_ACCESS_KEY,
+			service: 'lambda',
+		});
+
+		const submit_body = {
+			num1: 10,
+			num2: 10,
+		};
+
+		const lambdaResponse = await aws.fetch(env.LAMBDA_FUNCTION_URL, {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify(submit_body),
+			redirect: 'manual',
+		});
+
+		return lambdaResponse;
 	},
 };
