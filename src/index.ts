ca08a96e98d3e2d2b221a8ec044c6a8426a7f78b
Michael Wu <mw10013@gmail.com>
2024-03-22 09:01:08 -0400

    renderMediaOnLambda

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import { getFunctions, renderMediaOnLambda } from '@remotion/lambda/client';
+
 export interface Env {
 	REMOTION_AWS_ACCESS_KEY_ID: string;
 	REMOTION_AWS_SECRET_ACCESS_KEY: string;
@@ -5,11 +7,14 @@ export interface Env {
 
 export default {
 	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-		const result = 'remotion';
-		return new Response(
-			JSON.stringify({
-				result,
-			})
-		);
+		const result = await renderMediaOnLambda({
+			region: 'us-east-1',
+			functionName: 'remotion-render-bds9aab',
+			composition: 'MyVideo',
+			serveUrl: 'https://remotionlambda-qg35eyp1s1.s3.eu-central-1.amazonaws.com/sites/bf2jrbfkw',
+			codec: 'h264',
+		});
+
+		return new Response(JSON.stringify({ result }));
 	},
 };
